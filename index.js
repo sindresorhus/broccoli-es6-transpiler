99d5b36fe5d9af98bb387a2175f8504fb3647b21
Sindre Sorhus <sindresorhus@gmail.com>
2014-02-24 22:11:05 +0100

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+var chalk = require('chalk');
+var Filter = require('broccoli-filter');
+var es6transpiler = require('es6-transpiler');
+
+function Es6TranspilerFilter(inputTree, options) {
+	if (!(this instanceof Es6TranspilerFilter)) {
+		return new Es6TranspilerFilter(inputTree, options);
+	}
+
+	this.inputTree = inputTree;
+	this.options = options || {};
+
+	delete this.options.filename;
+	delete this.options.outputToConsole;
+	delete this.options.outputFilename;
+}
+
+Es6TranspilerFilter.prototype = Object.create(Filter.prototype);
+Es6TranspilerFilter.prototype.constructor = Es6TranspilerFilter;
+
+Es6TranspilerFilter.prototype.extensions = ['js'];
+Es6TranspilerFilter.prototype.targetExtension = 'js';
+
+Es6TranspilerFilter.prototype.processString = function (str, relativePath) {
+	this.options.src = str;
+
+	var result = es6transpiler.run(this.options);
+
+	if (result.errors.length > 0) {
+		return console.error(chalk.magenta('broccoli-es6-transpiler') + ' error in ' + chalk.blue(relativePath) + '\n' + result.errors.join('\n'));
+	}
+
+	return result.src;
+};
+
+module.exports = Es6TranspilerFilter;
---
+++ readme.md
@@ -0,0 +1,32 @@
+# [broccoli](https://github.com/joliss/broccoli)-es6-transpiler [![Build Status](https://travis-ci.org/sindresorhus/broccoli-es6-transpiler.png?branch=master)](https://travis-ci.org/sindresorhus/broccoli-es6-transpiler)
+
+> Transpile ES6 to ES5 with [es6-transpiler](https://github.com/termi/es6-transpiler)
+
+*Issues with the output should be reported on the es6-transpiler [issue tracker](https://github.com/termi/es6-transpiler/issues).*
+
+
+## Install
+
+```
+npm install --save broccoli-es6-transpiler
+```
+
+
+## Example
+
+```js
+var es6transpiler = require('broccoli-es6-transpiler');
+tree = es6transpiler(tree, options);
+```
+
+
+## API
+
+### es6transpiler(tree, options)
+
+Use the es6-transpiler [options](https://github.com/termi/es6-transpiler#options), except for `filename`, `src`, `outputToConsole`, `outputFilename`.
+
+
+## License
+
+MIT © [Sindre Sorhus](http://sindresorhus.com)
